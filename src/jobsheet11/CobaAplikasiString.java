5da65e9d0d37c14fc4fd902adc021307b1f64158
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-14 14:17:06 +0700

    Initial Commit

---
+++ src/jobsheet11/CobaAplikasiString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CobaAplikasiString {
+    public static void main(String[] args) {
+        String identitas = "M.Sultan Sahara/XR5/27";
+        System.out.println("Identitas siswa : " +identitas);
+        
+        
+    }
+    
+}
