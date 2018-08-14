aa300b6ee83229c06f2ce365ad91df599a4f5160
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-14 14:22:12 +0700

    Coba method length&isEmpty

--- src/jobsheet11/CobaAplikasiString.java
+++ src/jobsheet11/CobaAplikasiString.java
@@ -14,6 +14,15 @@ public class CobaAplikasiString {
         String identitas = "M.Sultan Sahara/XR5/27";
         System.out.println("Identitas siswa : " +identitas);
         
+        String x = "Operasi";
+        System.out.println("Isi Variabel x : "+ x);
+        System.out.println("\""+ x + "\" Panjang Karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : "+ x.isEmpty());
+        
+        String y = "";
+        System.out.println("Isi Variabel y : "+ y);
+        System.out.println("y adalah kosong : "+ y.isEmpty());
         
     }
     
